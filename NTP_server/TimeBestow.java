f9b734eae4b0b0f045366f7fb6666d87d4236976
elia renzoni <elia.renzoni03@gmail.com>
2024-07-25 10:38:56 +0200

    finish server side code

--- NTP_server/TimeBestow.java
+++ NTP_server/TimeBestow.java
@@ -4,15 +4,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
 import java.time.Instant;
+import java.util.Calendar;
 import java.util.concurrent.Callable;
 
 
 public class TimeBestow implements Callable<Void> {
     private Socket connection;
-    private Instant acceptionTimestamp;
-    private Instant replyTimestamp;
+    private String acceptionTimestamp;
+    private String replyTimestamp;
     private String senderTimestamp;
 
     public TimeBestow(final Socket conn) {
@@ -27,11 +30,11 @@ public class TimeBestow implements Callable<Void> {
         }
     }
     
-    private Instant timeStamp() {
-        return Instant.now(); 
+    private String timeStamp() {
+        return new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime()); 
     }
 
-    private void getTimestampFromRequest() {
+    private void unmarshaling() {
         try {
             var reader = new BufferedReader(new InputStreamReader(this.connection.getInputStream()));
             this.senderTimestamp = reader.readLine();
@@ -40,10 +43,30 @@ public class TimeBestow implements Callable<Void> {
         }
     }
 
+    private String marhsaling(final String t1, final String t2, final String t3) {
+        return t1 + "\n" + 
+               t2 + "\n" + 
+               t3 + "\n";
+    }
 
     @Override
-    public Void call() throws Exception {
+    public Void call() {
+        // setting up T2
+        this.acceptionTimestamp = this.timeStamp();
+        
+        // unmarshal the node message
+        this.unmarshaling();
 
-
+        // open the buffer and write the response
+        try {
+            var writer = new OutputStreamWriter(this.connection.getOutputStream());
+            this.replyTimestamp = this.timeStamp();
+            writer.write(this.marhsaling(senderTimestamp, replyTimestamp, acceptionTimestamp));
+        } catch (IOException e) {
+            System.out.println(e);
+        } finally {
+            this.closeConn();
+        }
+        return null;
     }
 }
