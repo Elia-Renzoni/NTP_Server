8009689015992375eb070c590f94482cf99acbae
elia renzoni <elia.renzoni03@gmail.com>
2024-07-25 11:03:42 +0200

    add more featurs to client ntp sourcesss

--- NTP_client/Client.java
+++ NTP_client/Client.java
@@ -1,5 +1,30 @@
 package NTP_client;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Client {
-    
+    public static void main(String ...args) {
+        NTPClient[] c = new NTPClient[60];
+
+        try {
+            Socket conn = new Socket("localhost", 4040);
+
+            for (int i = 0; i < c.length; i++) {
+                c[i] = new NTPClient(i, conn);
+                c[i].start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        for (int i = 0; i < c.length; i++) {
+            try {
+                c[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- NTP_client/NTPClient.java
+++ NTP_client/NTPClient.java
@@ -1,5 +1,45 @@
 package NTP_client;
 
-public class NTPClient {
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class NTPClient extends Thread {
+    private Socket conn;
+    private String beforeSendinTimeStamp;
+    private String acceptedServerConnTimeStamp;
+    private String whenSendingServerTimeStamp;
+    private String whenRecevingClientTimeStamp;
+
+    public NTPClient(final int tindex, final Socket connection) {
+        super("Client Number: " + tindex);
+        this.conn = connection;
+    }
+
+    private String getTimeStamp() {
+        return new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime()); 
+    }
     
+    @Override
+    public void run() { 
+        try {
+            var writer = new OutputStreamWriter(this.conn.getOutputStream());
+            writer.write(this.getTimeStamp() + "\n");
+
+            // now wait for the response...
+            var reader = new InputStreamReader(this.conn.getInputStream());
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                this.conn.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
